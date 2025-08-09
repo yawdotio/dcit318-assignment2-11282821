e5030452270361c8ef1abc0dfe782845e1ab7b6a
Yaw Bempong <yawadubempong@gmail.com>
2025-08-09 09:55:46 +0000

    Created animal classe to show inheritance and overidding

---
+++ Assignment2/Animal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+// This code demonstrates the use of inheritance. Task 1.
+public class Animal
+{
+    public virtual void MakeSound()
+    {
+        Console.WriteLine("Some generic sound");
+    }
+}
+
+public class Dog : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Bark");
+    }
+}
+
+public class Cat : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Meow");
+    }
+}
+
+
--- Assignment2/Program.cs
+++ Assignment2/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Drawing;
+
+public class Program
+{
+    public static void Main()
+    {
+        Animal genericAnimal = new Animal();
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+
+        // Task 1: Demonstrating inheritance and method overriding
+        // Each animal makes its own sound
+
+        Console.WriteLine("Animal Sounds:");
+        Console.WriteLine("-----------------");
+        Console.WriteLine("Generic Animal:");
+        genericAnimal.MakeSound(); // Output: Some generic sound
+        Console.WriteLine("Dog:");
+        dog.MakeSound();           // Output: Bark
+        Console.WriteLine("Cat:");
+        cat.MakeSound();  // Output: Meow
+
+
+    }
+}
